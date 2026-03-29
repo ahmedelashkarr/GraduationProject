58f4e5f0aaa247298c9389a3765e60a1b3468fc3
Ahmed Elashkar <ahmedelashkar243@.com>
2026-03-29 13:18:23 +0200

    first version for connect APIs in unity

--- dev/server/v1/arnavigation-springboot/arnavigation/src/main/java/com/arnav/controller/RoomsController.java
+++ dev/server/v1/arnavigation-springboot/arnavigation/src/main/java/com/arnav/controller/RoomsController.java
@@ -46,4 +46,5 @@ public class RoomsController {
             return ResponseEntity.internalServerError().body(Map.of("error", e.getMessage()));
         }
     }
+
 }
---
+++ dev/unity/Assets/Scripts/models/Room.cs
@@ -0,0 +1,10 @@
+using System;
+
+[Serializable]
+public class Room
+{
+    public string name;
+    public string zone;
+    public int floor;
+    public string type;
+}
